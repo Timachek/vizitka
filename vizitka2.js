c18bac5f9af046611b82168e40f93ddeb56aef9d
Timachek <lpv22229@gmail.com>
2024-12-29 23:54:45 +0300

    Add files via upload

---
+++ vizitka2.js
@@ -0,0 +1,4 @@
+document.getElementById('burgerMenu').addEventListener('click', function() {
+    const navbarLinks = document.getElementById('navbarLinks');
+    navbarLinks.classList.toggle('active'); // Добавляем/удаляем класс active
+});
