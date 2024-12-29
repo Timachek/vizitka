9e31b03669fc8e7aa6522abb18f2a8340fc0c90e
Timachek <lpv22229@gmail.com>
2024-12-30 02:13:31 +0300

    Update vizitka2.js

--- vizitka2.js
+++ vizitka2.js
@@ -1,4 +1,7 @@
-document.getElementById('burgerMenu').addEventListener('click', function() {
-    const navbarLinks = document.getElementById('navbarLinks');
-    navbarLinks.classList.toggle('active'); // Добавляем/удаляем класс active
-});
+document.getElementById('burgerMenu').addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+    const navbar = document.querySelector('.navbar');
+    navbar.classList.toggle('active');
+    console.log('Menu toggled'); // Для проверки, срабатывает ли функция
+}
